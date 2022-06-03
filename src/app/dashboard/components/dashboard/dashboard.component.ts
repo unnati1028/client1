7813badf56d541eb0136cef910a77f4fd049d73f
unnati1028 <62767100+unnati1028@users.noreply.github.com>
2022-06-03 10:05:56 -0500

    dashboard 10:05

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProfileService } from 'src/app/profile/services/profile.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private router: Router, private profileService: ProfileService) {}
+  //handling initialization of your component
+  //this rest call should happen inside the ngonit
   ngOnInit(): void {
+    this.profileService.getProfile().subscribe(
+      () => {},
+      (err) => {
+        console.log(JSON.stringify(err));
+      }
+    );
   }
-
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,15 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ProfileService } from '../profile/services/profile.service';
+import { httpInterceptor } from '../shared/interceptor';
 
 @NgModule({
-  declarations: [
-    DashboardComponent
-  ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+  declarations: [DashboardComponent],
+  imports: [CommonModule, HttpClientModule, DashboardRoutingModule],
+  providers: [ProfileService, httpInterceptor],
 })
-export class DashboardModule { }
+export class DashboardModule {}
