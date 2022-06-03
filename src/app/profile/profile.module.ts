a92ea8cb74f1efe3f7ecea22e1d86d80363dcb6c
unnati1028 <62767100+unnati1028@users.noreply.github.com>
2022-06-03 09:52:08 -0500

    profile update 9:51

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { httpInterceptor } from '../shared/interceptor';
+import { ProfileService } from './services/profile.service';
 
 @NgModule({
   declarations: [],
   imports: [HttpClientModule, CommonModule, ProfileRoutingModule],
+providers: [ProfileService,httpInterceptor]
 })
 export class ProfileModule {}
